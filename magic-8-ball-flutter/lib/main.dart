5a525a9ca77b852a410c9e2ba62f66b9bfe2ed87
Uday Kumar Madarapu <udaykumaruking@gmail.com>
2021-03-28 20:09:27 +0530

    Boss challenge accomplished

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Color.fromRGBO(0, 68, 167, 1),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int rander;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          onPressed: () {
+            setState(() {
+              rander = Random().nextInt(5) + 1;
+              print('Clicked');
+            });
+          },
+          child: Image.asset('images/ball$rander.png')),
+    );
+  }
+}
