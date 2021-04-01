8cdffa7f8f94050e88cf9d045ac374257bafb84d
Uday Kumar Madarapu <udaykumaruking@gmail.com>
2021-04-01 16:52:37 +0530

    Added geolocator and receving location

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +8,28 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    getLocation();
+    super.initState();
+  }
+
+  void getLocation() async {
+    print('sda');
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    // print('sdaasda');
+    print(position);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
+            print('object');
           },
           child: Text('Get Location'),
         ),
